4a3fe4ee699ee3ef286415fca3f2489d3a21a8d8
rza-git <123334643+rza-git@users.noreply.github.com>
2024-02-02 20:27:38 +0700

    feature register done (#1)

---
+++ register.js
@@ -0,0 +1,10 @@
+function register(username, password, age) {
+
+    console.log(username)
+    console.log(password)
+    console.log(age)
+
+    console.log("Account registered")
+}
+
+register("admin", "admin", 25)
